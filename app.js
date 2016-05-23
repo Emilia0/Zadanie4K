2e619c12a0496cddb7489c234b8e4ed74360acd8
Emilia Kamrowska <emilia.kamrowska@o2.pl>
2016-05-23 23:33:29 +0200

    data (json &xml) added

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 (function () {
-    var app = angular.module('fundCompare',[]);
+    //var app = angular.module('fundCompare',[]);
+
+    $.ajax({
+        type: "GET",
+        url: "data/data.json",
+        dataType: "json",
+        error: function() {
+            console.log('Opps error');
+        },
+        success: function(response){
+            var dateArray = [];
+
+            //console.log(response);
+            dateArray.push(Object.getOwnPropertyNames(response));
+            //console.log(dateArray.length);
+        }
+    })
 
 
 
 
 
-
-
-
-});
+})();
